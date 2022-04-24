9aa68c3e5bc876410c7fc866abcae6c093605066
gimhafnu <60770867+gimhafnu@users.noreply.github.com>
2022-04-24 23:28:10 +0700

    Tries to fix and perfect my scripts
    I'm sorry if it messes with stuffs, but this should be good now

--- src/get_most_similar_image.m
+++ src/get_most_similar_image.m
@@ -5,32 +5,34 @@ function [char, similarity] = get_most_similar_image(image, method)
     end
 
     fonts_folder = "..\image\templates\fonts_seg\";
-    fonts = ["A","B","C","H","I","J","L","U","0","1","2","4", ...
-    "6","8"];
+    %fonts = ["A","B","C","H","I","J","L","U","0","1","2","4", ...
+    %"6","8"];
+    fonts = ["A","B","C","D","E","F","G","H","I","J","K","L","M", ...
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z", ...
+    "0","1","2","3","4","5","6","7","8","9"];
     file_type = ".png";
 
     most_similar = "_";
     most_similar_score = 0;
     
     % image MUST be a binary image, but considering the weird ways they are
-    % saved...
-    %image_BW = imbinarize(image);
+    % saved... try to handle those too anyway
+
+    % If the image is saved RGB, then make it grayscale first
     if (size(image,3)) == 3
             image = rgb2gray(image);
     end
-    %disp(class(I));
+    
+    % If the image isn't a binary image, then make it one
     if islogical(image)
         image_BW = image;
     else
         image_BW = imbinarize(image);
     end
-    %image_BW = image;
-    %image_skeleton = bwmorph(image_BW,'spur',Inf);
-    image_skeleton = bwmorph(image_BW,'skel',inf); % Makes the skeleton
-    %image_skeleton = bwmorph(image_skeleton,'skel',inf); % Makes the skeleton
-    %image_skeleton = bwmorph(image_skeleton,'spur',Inf);
-
     
+    image_skeleton = bwmorph(image_BW,'skel',inf); % Makes the skeleton
+
+    % Readies folder to save the image's skeleton
     if not(isfolder("..\image\temporary\temp_"))
         mkdir ..\image\temporary\temp_
     end
@@ -43,6 +45,7 @@ function [char, similarity] = get_most_similar_image(image, method)
             cnt_num_helper = true;
         end
     end
+    % Writes the image skeleton
     imwrite(image_skeleton, "..\image\temporary\temp_\main_bw" + cnt_num + ".png");
 
     temp_bak = [];
@@ -51,6 +54,8 @@ function [char, similarity] = get_most_similar_image(image, method)
         full_path = fonts_folder + fonts(i) + file_type;
         %disp(full_path);
         I = imread(full_path);
+
+        % Handles the weirdly saved font files too (makes sure it's binary)
         if (size(I,3)) == 3
             I = rgb2gray(I);
         end
@@ -60,13 +65,15 @@ function [char, similarity] = get_most_similar_image(image, method)
         else
             I_BW = imbinarize(I);
         end
-        %I_BW_skel = bwmorph(I_BW,'spur',Inf);
-        I_BW_skel = bwmorph(I_BW,'skel',inf); % Makes the skeleton
-        %I_BW_skel = bwmorph(I_BW_skel,'skel',inf); % Makes the skeleton
-        %I_BW_skel = bwmorph(I_BW_skel,'spur',Inf);
         
+        I_BW_skel = bwmorph(I_BW,'skel',inf); % Makes the skeleton
         
         %figure, imshow(I_BW_skel);
+
+        % Get the similary score with the current character based on
+        % selected method
+        % % ssim is... structural similarity... it's kinda similar to PSNR,
+        % I think???
         if method == "skeleton"
             sim_with_current = ssim(uint8(image_skeleton), uint8(I_BW_skel));
             %sim_with_current = psnr(uint8(image_skeleton), uint8(I_BW_skel));
@@ -83,16 +90,21 @@ function [char, similarity] = get_most_similar_image(image, method)
             sim_with_current = (sim_1 + sim_2) / 2;
         end
         %disp("  "), disp(fonts(i)), disp(" : "), disp(sim_with_current);
+
+        % If the score is better than the currently saved best one... then
+        % make this the best one
         if sim_with_current > most_similar_score
             most_similar_score = sim_with_current;
             most_similar = fonts(i);
         end
 
         temp_bak = [temp_bak, [fonts(i), sim_with_current]];
+
+        % saves the skeleton
         imwrite(I_BW_skel, "..\image\temporary\temp_\" + fonts(i) + "_bw" + ".png");
     end
 
-    disp(temp_bak);
+    %disp(temp_bak);
 
     char = most_similar;
     similarity = most_similar_score;
--- src/segment_image.m
+++ src/segment_image.m
@@ -1,9 +1,4 @@
 % Attempts to segment the images from a platenumber
-%% Masih bermasalah
-% 1. Garis yang pemisah plat nomor dan tanggal kadaluarsa
-% 2. Kalau di bagian luarnya masih ada padding
-% % Kayaknya yang ke 2 ini bisa diselesaikan dengan pakai edge detection?
-%%
 
 function [list_of_files] = segment_image(image, folder, file_name, num_suffix)
     if nargin==2
@@ -23,10 +18,13 @@ function [list_of_files] = segment_image(image, folder, file_name, num_suffix)
     % cleans the image from small stuffs/area
     I_bin_clean = I_bin;
     
+    % This is for the output, to perhaps help other functions know which
+    % files got created from this segmentation
     seg_file_list = [];
     
     
     % Attempts to check if the background is white or black
+    
     white_count = 0;
     black_count = 0;
     for i = 1:I_size(1)
@@ -40,11 +38,13 @@ function [list_of_files] = segment_image(image, folder, file_name, num_suffix)
             %end
         end
     end
-    
+    % If the background is white, make it black
     if white_count > black_count
         I_bin_clean = ~I_bin_clean;
     end
     
+    % Tries to clean smaller "areas" from the image
+    % This should delete the expiration date
     I_bin_clean =  bwareaopen(I_bin_clean, round(I_size(1) * I_size(2) / 160));
     %I_bin_clean =  bwareaopen(I_bin_clean, 128);
     
@@ -54,59 +54,54 @@ function [list_of_files] = segment_image(image, folder, file_name, num_suffix)
     [Labels, label_n] = bwlabel(I_bin_clean);
     Label_props = regionprops(Labels,'BoundingBox');
     % Tries to make segmentations' bounding box
-    hold on
-    for n=1:size(Label_props,1)
-        rectangle('Position',Label_props(n).BoundingBox,'EdgeColor','g','LineWidth',2)
-    end
-    hold off
+    %hold on
+    %for n=1:size(Label_props,1)
+    %    rectangle('Position',Label_props(n).BoundingBox,'EdgeColor','g','LineWidth',2)
+    %end
+    %hold off
 
     %figure;
 
     % Segments the image
     for n=1:label_n
+        % Gets the actual label's area by checking which pixels have each
+        % label's number
         [r,c] = find(Labels==n);
         n1 = I_bin_clean(min(r):max(r),min(c):max(c));
         %n1 = imgaussfilt(double(n1),1);
+
+        % Attempts to smooth out the edges of the area
         N = 6;
         kernel = ones(N, N) / N^2;
         blurryImage = convn(double(n1), kernel, 'same');
         n1 = blurryImage > 0.5;
-
+        
+        % Attempts to upscale the image
         minpadsize = 4; 
-        outsize = [256 256];
-        nr = imresize(n1,[outsize(1)-2*minpadsize NaN],'bicubic');
-        hpad = (outsize(2)-size(nr,2))/2;
-        if hpad < 0
+        outsize = [256 256]; % The actual image size to be made
+        n1_size = size(n1);
+        
+        % Ignores area if it's... long
+        % More or less the aspect ratio of the actual plate
+        if n1_size(2) / n1_size(1) > 2.4
             continue
         end
-        %np_pre = [minpadsize floor(hpad)];
-        %np = padarray(nr,[minpadsize floor(hpad)],0,'pre'); 
-        %np = padarray(np,[minpadsize ceil(hpad)],0,'post');
-        %np = imgaussfilt(double(np),1);
 
+        nr = [];
+        hpad = 0;
+        % Resizes the image
         n1 = imresize(n1,outsize);
         n1 = imgaussfilt(double(n1),1);
         n1 = padarray(imresize(n1,[20 20],'bicubic'),[4 4],0,'both'); 
         np = n1;
 
-        %np_old = np;
-        %[nx,ny] = size(np_old) ;
-        %[y,x] = find(np_old(:,:,1)==1) ;
-        %np = zeros(nx,ny) ;
-        %[X,Y] = meshgrid(1:ny,1:nx) ;
-        %idx = boundary(x,y) ;
-        %idx = inpolygon(X(:),Y(:),x(idx),y(idx)) ;
-        %np(idx) = 1 ;
-
-        
-        
-        %subplot(3, 4, n);
-        %imshow(np);
-        
+        % Makes the segmentation folder if it doesn't exist
         if not(isfolder(folder))
             mkdir(folder);
         end
         segmentedImages1 = [];
+
+        % Writes the segmented images
         if num_suffix
             segmentedImages1 = fullfile(folder, sprintf(file_name+'%d.png', n));
         else
